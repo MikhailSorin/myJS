9e9a52761a400f2d570e877c7f53957a1668915b
MikhailSorin <sorin.michael@gmail.com>
2022-11-15 19:25:02 +0200

    Update 21script.js
    test

--- 21script.js
+++ 21script.js
@@ -1,6 +1,6 @@
 function removeChar(str) {
   console.info(
     str.split("").slice(1).reverse("").slice(1).reverse("").join("")
-  );
+  ); //test
   return str.split("").slice(1).reverse("").slice(1).reverse("").join("");
 }
