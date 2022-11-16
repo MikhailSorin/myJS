c789e4eb9c368c097566f76de4cdd6e0e9aa9842
MikhailSorin <sorin.michael@gmail.com>
2022-11-16 19:36:57 +0200

    Create 30script.js
    easy but isn't short

---
+++ 30script.js
@@ -0,0 +1,11 @@
+function monkeyCount(n) {
+  var arr1 = [];
+  for (let i = 1; i <= n; i++) {
+    arr1[i - 1] = i;
+  }
+
+  return arr1;
+  // your code here
+}
+
+//   const monkeyCount=(n)=>arr1[var i]=;
