1e9276cc7f29a1f5317ea70723d3b93029ae33ad
MikhailSorin <sorin.michael@gmail.com>
2022-11-17 20:21:30 +0200

    Create 31script.js
    I'll do it later

---
+++ 31script.js
@@ -0,0 +1,17 @@
+const flip = (d, a) => {
+  if (d == "R") {
+    for (let i = a.length; i >= 0; i--) {
+      if (a[i - 1] > a[i]) {
+        //сыпимся в право
+      }
+      const element = array[i];
+    }
+  } else if (d == "L") {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i]) {
+      }
+      const element = array[i];
+    }
+  }
+  return;
+};
