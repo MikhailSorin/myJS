897fcc44962f93661072d30d968ae6593b78557c
MikhailSorin <sorin.michael@gmail.com>
2022-11-25 22:01:30 +0200

    Create 9script.js
    it can be refracted

---
+++ 9script.js
@@ -0,0 +1,24 @@
+function isIsogram(str) {
+  let collectionCopy = str.toLowerCase();
+  let count = 0;
+  let max = 0;
+  console.info("start" + collectionCopy);
+  for (let i = 0; i < collectionCopy.length; i++) {
+    for (let j = 0; j < collectionCopy.length; j++) {
+      if (collectionCopy[i] == collectionCopy[j]) {
+        count = count + 1;
+        console.info(collectionCopy);
+      }
+      if (max < count) {
+        max = count;
+        console.info("max+=" + max);
+      }
+    }
+    console.info(collectionCopy);
+    count = 0;
+  }
+  console.info("max=" + max);
+
+  if ((max == 1) | (str = NaN)) return true;
+  return false;
+}
