e2106134644f55bdc425822796e3332777a948d2
MikhailSorin <sorin.michael@gmail.com>
2023-01-18 21:35:38 +0200

    Today activities

---
+++ 14.js
@@ -0,0 +1,7 @@
+function aliasGen(a, b) {
+    let nameFirstLetter = a[0].toUpperCase()
+    let sernameFirstLetter = b[0].toUpperCase()
+    console.log(firstName[nameFirstLetter])
+    console.log(surname[sernameFirstLetter])
+    return firstName[nameFirstLetter]+' '+surname[sernameFirstLetter]
+}
