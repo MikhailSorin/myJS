fb067b85104b92d75a64834678e9e6a87f8f2942
MikhailSorin <sorin.michael@gmail.com>
2023-01-23 21:34:03 +0200

    Today activities

--- 14.js
+++ 14.js
@@ -3,5 +3,6 @@ function aliasGen(a, b) {
     let sernameFirstLetter = b[0].toUpperCase()
     console.log(firstName[nameFirstLetter])
     console.log(surname[sernameFirstLetter])
-    return firstName[nameFirstLetter]+' '+surname[sernameFirstLetter]
+    if ((firstName[nameFirstLetter] == undefined) || (surname[sernameFirstLetter] == undefined)) return "Your name must start with a letter from A - Z."
+    return firstName[nameFirstLetter] + ' ' + surname[sernameFirstLetter]
 }
